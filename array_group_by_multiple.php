40439a791a572f63847c4e58f45296ad6aa4b4ff
micheled <me@thisisdurante.com>
2019-05-31 21:52:44 +0200

    Adds implementation and tests forgrouping by multiple columns

---
+++ array_group_by_multiple.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Groups sorted rows of an array with a stream aggregate function into a single row for each group of rows that have
+ * the same value for the provided columns. If no user function is provided, the first matching item in a group will be
+ * returned.
+ *
+ * Note that array_group_by() expects the array rows to be sorted on the columns to be used for grouping and columns are
+ * checked strictly.
+ *
+ * @param array $columns
+ * @param array $array
+ * @param callable|null $group_func
+ * @return array
+ */
+
+function array_group_by_multiple(array $columns, array $array, callable $group_func = null): array
+{
+    if (empty($array)) return [];
+
+    $groups = [];
+    $bucket[] = $array[0]; // initialize bucket
+
+    for ($i = 1, $max = count($array); $i < $max; $i++) {
+        $match = false;
+
+        foreach ($columns as $column) {
+            if ($array[$i][$column] === $array[$i - 1][$column]) {
+                $match = true;
+            } else {
+                $match = false;
+                break; // all columns must be equal
+            }
+        }
+
+        if ($match) {
+            $bucket[] = $array[$i];
+            continue;
+        } else {
+            $groups[] = $group_func !== null ? $group_func($bucket) : $bucket[0];
+            $bucket = [];
+            $bucket[] = $array[$i];
+        }
+    }
+
+    $groups[] = $group_func !== null ? $group_func($bucket) : $bucket[0];
+    return $groups;
+}
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,4 @@
 <?php
 
 require_once 'array_group_by.php';
+require_once 'array_group_by_multiple.php';
